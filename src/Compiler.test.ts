4b03de7468cb8168b3c0c0f8d61a3006c7a45924
Groundwater <groundwater@gmail.com>
2021-03-07 00:30:35 -0800

    Compiler

--- src/Compiler.test.ts
+++ src/Compiler.test.ts
@@ -86,39 +86,53 @@ export class JSHeader {
     ) { }
 }
 export abstract class NodeBody {
-    abstract Write(compiler: Compiler, prefix: string): Generator<string>
+    abstract Write(compiler: Compiler, index: number): Generator<string>
 }
 export class ExpressionBody extends NodeBody {
     constructor(
         public UNSAFE_body: string
     ) { super() }
-    *Write(compiler: Compiler, prefix: string) {
-        let o = this.UNSAFE_body.replaceAll(/\$(\d+)/g, `${prefix}_$1`)
-        yield compiler.WrapStatement(`${prefix} = (${o})`)
+    *Write(compiler: Compiler, index: number) {
+        let out = []
+        let line = this.UNSAFE_body
+        while (true) {
+            let next = line.match(/\$\d+?/)
+            if (!next) break
+            let { index: match_index, 0: match } = next
+            assert(match_index !== undefined)
+            out.push(line.substr(0, match_index))
+            let sub = line.substring(match_index + 1, match_index + 1 + match.length)
+            let slot = parseInt(sub)
+            assert(isFinite(slot))
+            out.push(compiler.Var(index, slot))
+            line = line.substr(match_index + match.length)
+        }
+        out.push(line)
+        yield compiler.WrapStatement(`${compiler.Var(index)} = (${out.join('')})`)
     }
 }
 export class InfixOperatorBody extends NodeBody {
     constructor(
         public infix: string
     ) { super() }
-    *Write(compiler: Compiler, prefix: string) {
-        yield compiler.WrapStatement(`${prefix} = (${prefix}_0 ${this.infix} ${prefix}_1)`)
+    *Write(compiler: Compiler, index: number) {
+        yield compiler.WrapStatement(`${compiler.Var(index)} = (${compiler.Var(index, 0)} ${this.infix}${compiler.Var(index, 1)})`)
     }
 }
 export class PrefixyOperatorBody extends NodeBody {
     constructor(
         public prefix: string
     ) { super() }
-    *Write(compiler: Compiler, prefix: string) {
-        yield compiler.WrapStatement(`${prefix} = (${this.prefix}${prefix}_0)`)
+    *Write(compiler: Compiler, index: number) {
+        yield compiler.WrapStatement(`${compiler.Var(index)} = (${this.prefix}${compiler.Var(index, 0)})`)
     }
 }
 export class PostfixOperatorBody extends NodeBody {
     constructor(
         public postfix: string
     ) { super() }
-    *Write(compiler: Compiler, prefix: string) {
-        yield compiler.WrapStatement(`${prefix} = (${prefix}_0${this.postfix})`)
+    *Write(compiler: Compiler, index: number) {
+        yield compiler.WrapStatement(`${compiler.Var(index)} = (${compiler.Var(index, 0)}${this.postfix})`)
     }
 }
 export class CallableBody extends NodeBody {
@@ -126,20 +140,20 @@ export class CallableBody extends NodeBody {
         public body: string,
         public argsN: number = 0,
     ) { super() }
-    *Write(compiler: Compiler, prefix: string) {
+    *Write(compiler: Compiler, index: number) {
         let args = []
         for (let i = 0; i < this.argsN; i++) {
-            args.push(`${prefix}_${i}`)
+            args.push(`${compiler.Var(index, i)}`)
         }
-        yield compiler.WrapStatement(`${prefix} = (${this.body}(${args.join(', ')}))`)
+        yield compiler.WrapStatement(`${compiler.Var(index)} = (${this.body}(${args.join(', ')}))`)
     }
 }
 export class LiteralBody extends NodeBody {
     constructor(
         public value: any
     ) { super() }
-    *Write(compiler: Compiler, prefix: string) {
-        yield compiler.WrapStatement(`${prefix} = (${this.value})`)
+    *Write(compiler: Compiler, index: number) {
+        yield compiler.WrapStatement(`${compiler.Var(index)} = (${this.value})`)
     }
 }
 export class Nodes {
@@ -203,7 +217,7 @@ export class ReverseNonControlInput extends NonControlInput {
 }
 export class ControlInput extends Input {
     *WriteDeclare(compiler: Compiler, local: number, slot: number) {
-        yield compiler.WrapStatement(`var ${compiler.VarIn(local)}`)
+        yield compiler.WrapStatement(`var ${compiler.Var(local, slot)}`)
     }
     *Write(compiler: Compiler, local: number, slot: number) {
         yield compiler.WrapStatement(`${compiler.Var(local, slot)} = ${compiler.VarIn(local)}`)
@@ -242,7 +256,7 @@ export class ForwardNode extends Node {
             }
 
             // Body
-            yield* this.body.Write(caseCompiler, name)
+            yield* this.body.Write(caseCompiler, index)
 
             // Output
             if (this.output) {
@@ -259,7 +273,8 @@ export class ForwardNode extends Node {
     * WriteDeclareNode(compiler: Compiler, index: number) {
         assert(index > -1)
         let name = compiler.Var(index)
-        yield compiler.WrapStatement(`var ${name}`)
+        yield compiler.WrapStatement(`var ${compiler.Var(index)}`)
+        yield compiler.WrapStatement(`var ${compiler.VarIn(index)}`)
         for (let [slot, nci] of enumerate(this.non_control_inputs)) {
             yield* nci.WriteDeclare(compiler, index, slot)
         }
@@ -280,7 +295,7 @@ export class ReverseNode extends Node {
                 yield* ncis.WriteDeclare(bodyCompiler, index, slot)
                 yield* ncis.Write(bodyCompiler, index, slot)
             }
-            yield* this.body.Write(bodyCompiler, name)
+            yield* this.body.Write(bodyCompiler, index)
             yield bodyCompiler.WrapStatement(`return ${name}`)
         }
         yield compiler.WrapStatement(`}`)
